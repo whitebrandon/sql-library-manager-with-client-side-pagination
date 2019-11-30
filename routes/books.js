f28c5033e2d1ede0cb966f73e5d5147bf6e4746e
Brandon White <white.brandonsean@gmail.com>
2019-11-30 14:48:12 -0500

    removed migrations, added to db, updated styles, routes, and views

--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const { Book } = require('../models');
+const Sequelize = require('sequelize');
+const { Op } = Sequelize
 
 /* READs full list of books. */
 router.get('/', async (req, res) => {
-  try {
-    const books = await Book.findAll();
-    res.render('index', {books: books.map(book => book = book.dataValues)});
-  } catch (error) {
-    console.error(error);
-  }
+  res.redirect('/books/pg/1')
 });
+
+router.post('/', async (req, res) => {
+  const books = await Book.findAll({
+    where: {
+      [Op.or]: [
+        {
+          title: {
+            [Op.like]: `%${req.body.search}%`,
+          }
+        }, 
+        {
+          author: {
+            [Op.like]: `%${req.body.search}%`,
+          }
+        }, 
+        {
+          genre: {
+            [Op.like]: `%${req.body.search}%`,
+          }
+        }, 
+        {
+          year: {
+            [Op.like]: `%${req.body.search}%`,
+          }
+        }]
+    }
+  })
+  res.render('index2', {books: books.map(book => book = book)});
+})
+
+router.get('/pg/:id', async (req, res) => {
+  console.log(req.body);
+  const length = await Book.findAll()
+  const books = await Book.findAll({offset: (req.params.id * 10) - 10 , limit: 10})
+  res.render('index', {books: books.map(book => book = book), length: length.length, active: parseInt(req.params.id)});
+})
 
 /* READs the "create a new book" form. */
 router.get('/new', (req, res) => {
@@ -60,13 +93,24 @@ router.post('/:id', async (req, res) => {
 });
 
 /* DELETEs a book */
-router.post('/:id/delete', async (req, res) => {
+router.get('/:id/delete', async (req, res) => {
+  try {
+    let book = await Book.findByPk(req.params.id);
+    book = book.dataValues
+    // await Book.destroy({where: {id: book.id}});
+    res.render('delete-confirm', {book})
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post('/:id/confirm', async (req, res) => {
   try {
     const book = await Book.findByPk(req.params.id);
     await Book.destroy({where: {id: book.id}});
-    res.redirect('/books')
+    res.redirect('/books');
   } catch (error) {
-    console.error(error);
+    console.error("Oops, there was an error: ", error);
   }
 });
 
